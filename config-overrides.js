77ca7feb92444cd9c08d6d051492ab0fa63eb827
rajat varshney <61705658+Er-rajat-2003@users.noreply.github.com>
2025-04-09 01:09:25 +0530

    Initial commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,23 @@
+// config-overrides.js
 const webpack = require('webpack');
 
 module.exports = function override(config) {
     config.resolve.fallback = {
         ...config.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        zlib: require.resolve('browserify-zlib'),
-        url: require.resolve('url'),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
+        crypto: require.resolve("crypto-browserify"),
+        https: require.resolve("https-browserify"),
+        url: require.resolve("url/"),
     };
+
     config.plugins = [
         ...config.plugins,
         new webpack.ProvidePlugin({
-            process: 'process/browser',
             Buffer: ['buffer', 'Buffer'],
+            process: 'process/browser',
         }),
     ];
+
     return config;
 };
