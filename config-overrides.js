e2a3571319204bf7487df05a2f0fab1ff61bd11e
rajat varshney <61705658+Er-rajat-2003@users.noreply.github.com>
2025-04-10 12:39:56 +0000

    first commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,28 @@
 const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = function override(config) {
-    config.resolve.fallback = {
-        ...config.resolve.fallback,
-        buffer: require.resolve('buffer/'),
-        stream: require.resolve('stream-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        https: require.resolve('https-browserify'),
-        http: require.resolve('stream-http'),
-        zlib: require.resolve('browserify-zlib'),
-        url: require.resolve('url/'),
-        path: require.resolve('path-browserify'),
-        vm: require.resolve('vm-browserify'), // 👈 Add this line
-    };
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    buffer: require.resolve('buffer/'),
+    stream: require.resolve('stream-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    https: require.resolve('https-browserify'),
+    http: require.resolve('stream-http'),
+    zlib: require.resolve('browserify-zlib'),
+    url: require.resolve('url/'),
+    path: require.resolve('path-browserify'),
+    vm: require.resolve('vm-browserify'), // This line stays
+  };
 
-    config.plugins = [
-        ...config.plugins,
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-            process: 'process/browser',
-        }),
-    ];
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+    new NodePolyfillPlugin(), 
+  ];
 
-    return config;
+  return config;
 };
